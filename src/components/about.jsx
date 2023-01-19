c43a57e5c6bb2f5725b9e08fd9158c090b778a43
ian <mccleai@wwu.edu>
2023-01-18 16:32:37 -0800

    first

---
+++ src/components/about.jsx
@@ -0,0 +1,76 @@
+import React, { Component } from 'react'
+import { DiReact } from "react-icons/di";
+import { DiAngularSimple } from "react-icons/di";
+import { DiHtml5 } from "react-icons/di";
+
+export default class About extends Component {
+  render() {
+    return (
+      <div>
+        <section className="colorlib-about" data-section="about">
+        <div className="colorlib-narrow-content">
+            <div className="row">
+            <div className="col-md-12">
+                <div className="row row-bottom-padded-sm animate-box" data-animate-effect="fadeInLeft">
+                <div className="col-md-12">
+                    <div className="about-desc">
+                    <span className="heading-meta">About Us</span>
+                    <h2 className="colorlib-heading">Who Am I?</h2>
+                    <p>I am a computer science graduate from Western Washington University living in the Pacific Northwest. My main experience is in web and application development, although I have dabbled in machine learning as well.</p>
+                    <p>My hobbies outside of programming include biking and skiing in the Sawtooth and Costal mountains. I'm lucky to have such amazing scenery in my backyard.</p>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+        </section>
+        <section className="colorlib-about">
+        <div className="colorlib-narrow-content">
+            <div className="row">
+            <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+                <span className="heading-meta">What I do?</span>
+                <h2 className="colorlib-heading">Here are some of my expertise</h2>
+            </div>
+            </div>
+            <div className="row row-pt-md">
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-1">
+                <span className="icon">
+                    <i className="icon-bulb" />
+                </span>
+                <div className="desc">
+                    <h3>Web Development </h3>
+                    <p>My experience building websites is in both React <DiReact /> and AngularJS <DiAngularSimple />, accompanied by HTML and CSS.</p>
+                </div>
+                </div>
+            </div>
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-3">
+                <span className="icon">
+                    <i className="icon-phone3" />
+                </span>
+                <div className="desc">
+                    <h3>Data Structures & Algorithms</h3>
+                    <p>Coming from a CS background, I have good grasp over fundamental concepts of DSA</p>
+                </div>
+                </div>
+            </div>
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-5">
+                <span className="icon">
+                    <i className="icon-data" />
+                </span>
+                <div className="desc">
+                    <h3>Machine Learning</h3>
+                    <p>I have implemented and trained various regression algorithms using hyperparameter tuning in the field of chemistry.</p>
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+        </section>
+      </div>
+    )
+  }
+}
---
+++ src/components/sidebar.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+import {DiReact} from "react-icons/di"
+
+export default class Sidebar extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <nav href="#navbar" className="js-colorlib-nav-toggle colorlib-nav-toggle" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"><i /></nav>
+          <aside id="colorlib-aside" className="border js-fullheight">
+            <div className="text-center">
+              <div className="author-img" style={{backgroundImage: 'url(images/about.jpg)'}} />
+              <h1 id="colorlib-logo"><a href="index.html">Ian McCleary</a></h1>
+              <span className="email"><i className="icon-mail"></i> m.c.leary8768@gmail.com</span>
+            </div>
+            <nav id="colorlib-main-menu" role="navigation" className="navbar">
+              <div id="navbar" className="collapse">
+                <ul>
+                  <li className="active"><a href="#home" data-nav-section="home">Introduction</a></li>
+                  <li><a href="#about" data-nav-section="about">About</a></li>
+
+                  <li><a href="#timeline" data-nav-section="timeline">Timeline</a></li>
+                  <li><a href="#projects" data-nav-section="projects">Projects</a></li>
+                </ul>
+              </div>
+            </nav>
+            <nav id="colorlib-main-menu">
+              <ul>
+                <li><a href="https://www.linkedin.com/in/ian-mccleary-672b23221/" target="_blank" rel="noopener noreferrer"><i className="icon-linkedin2" /></a></li>
+                <li><a href="https://github.com/ian-mccleary" target="_blank" rel="noopener noreferrer"><i className="icon-github"></i></a></li>
+              </ul>
+            </nav>
+            <div className="colorlib-footer">
+              <p><small>
+                  Made with React <DiReact /><br></br>
+              </small></p>
+            </div>
+          </aside>
+        </div>
+      </div>
+    )
+  }
+}
